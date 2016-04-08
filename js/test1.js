6b69a1a58de9b763d305a3fce9a5e7110013bf76
shuffleldliu <shuffleldliu@sohu-inc.com>
2016-04-08 16:26:20 +0800

    update

--- js/test1.js
+++ js/test1.js
@@ -19,14 +19,21 @@ var global_model = angular.module('MyApp',[]);
 global_model.controller('Data_res',function($scope){
 		$scope.global_val = global_val;
 	$scope.vals = [
-				{title:"刘迪的百科全书",app:9,price:100},
-				{title:"钢铁是怎么炼成的",app:2,price:34},
-				{title:"维基百科",app:3,price:15},
-				{title:"你好——word",app:7,price:068}
+				{title:"刘迪的百科全书",app:1,price:100},
+				{title:"钢铁是怎么炼成的",app:1,price:34},
+				{title:"维基百科",app:1,price:15},
+				{title:"你好——word",app:1,price:068}
 				];
 	$scope.remove = function(i){
 		$scope.vals.splice(i,1);
 	}
 });
+global_model.controller('FormTest',function($scope){
+	$scope.check_in  = false;
+	$scope.do_change = function(){
+		$scope.a = $scope.do_model.bb * 2;
+	}
+	$scope.do_model = {bb:0}
+})
 
 
